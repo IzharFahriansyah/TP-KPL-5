540e809390b7d67c4807d39dad0f58b3b38fbf25
izharfahriansyah <izharfahriansyah7@gmail.com>
2024-03-18 02:42:08 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul5_1302220056/Program.cs
+++ tpmodul5_1302220056/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace tpmodul5_1302220056
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302220056
 {
-    internal class Program
+    public class HaloGeneric
+    {
+        public void SapaUser<T>(T userName)
+        {
+            Console.WriteLine($"Halo user {userName}");
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HaloGeneric halo = new HaloGeneric();
+
+
+            string namaPanggilan = "Izhar";
+            halo.SapaUser(namaPanggilan);
+            Console.Read();
         }
     }
 }
