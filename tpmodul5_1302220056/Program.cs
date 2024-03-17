d523ceabc2dbc16790ceb5df77262e5fd3474478
izharfahriansyah <izharfahriansyah7@gmail.com>
2024-03-18 02:45:47 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302220056/Program.cs
+++ tpmodul5_1302220056/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace tpmodul5_1302220056
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302220056
 {
-    internal class Program
+    class DataGeneric<T>
     {
+        T Data { get; set; }
+        public DataGeneric(T data)
+        {
+            Data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+        }
+    }
+    class Program
+    {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataGeneric<string> stringData = new DataGeneric<string>("Y");
+            stringData.PrintData();
+            DataGeneric<string> nimData = new DataGeneric<string>("1302220056");
+            nimData.PrintData();
+            Console.ReadLine();
         }
     }
 }
